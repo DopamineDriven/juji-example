b8e12b030e61431102bccf48b470acd14dcdc8a3
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-11-08 23:40:38 -0600

    mongo remote success -- codegen via gql-let and graphql-codegen

--- dao/mongo.ts
+++ dao/mongo.ts
@@ -1,29 +1,20 @@
 import { MongoClient, Db } from 'mongodb';
 
-const { DB_URI, DB_TARGET } = process.env;
-
-if (!DB_URI) {
-  throw new Error('Please define the DB_URI variable in .env.local')
-}
-
-if (!DB_TARGET) {
-  throw new Error('Please define the DB_TARGET inside .env.local')
-}
-
 export let client: MongoClient;
 export let database: Db;
 
-const url = DB_URI;
+const url = process.env.DB_URI;
 
 const ConnectDatabase = async (): Promise<Db> => {
   if (!database) {
     console.info(`connecting to database...`)
-    client = await MongoClient.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-      database = client.db(DB_TARGET)
   }
+  client = await MongoClient.connect(url ?? '', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+      database = client.db(process.env.DB_TARGET ?? '')
+  
   return database;
 }
 
